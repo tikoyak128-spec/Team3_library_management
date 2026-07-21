00e8d004c21194de980576159e3de930381b573a
TolPanhrith <propanharith@gmail.com>
2026-07-21 20:16:54 +0700

    Complete authentication module

--- Authentication/login.php
+++ Authentication/login.php
@@ -29,7 +29,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <meta charset="UTF-8">
 <title>Login — Library Pro</title>
 <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
-<link rel="stylesheet" href="../Assets/css/style.css">
+<link rel="stylesheet" href="style.css">
+
 </head>
 <body>
 <div class="auth-wrapper">
--- Authentication/register.php
+++ Authentication/register.php
@@ -39,7 +39,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <meta charset="UTF-8">
 <title>Register — Library Pro</title>
 <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
-<link rel="stylesheet" href="../Assets/css/style.css">
+<link rel="stylesheet" href="style.css">
+
 </head>
 <body>
 <div class="auth-wrapper">
